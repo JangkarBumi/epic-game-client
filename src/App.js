332e235cdc257d0d94e164530edc8140bef2950d
Zeha Irawan <zehairawan@gmail.com>
2021-11-27 07:52:48 +0800

    attack boss UI

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SelectCharacter from './Components/SelectCharacter';
 import { CONTRACT_ADDRESS, transformCharacterData } from './constants';
 import myEpicGame from './utils/MyEpicGame.json';
 import { ethers } from 'ethers';
+import Arena from './Components/Arena';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -65,6 +66,10 @@ const [characterNFT, setCharacterNFT] = useState(null);
        */
     } else if (currentAccount && !characterNFT) {
       return <SelectCharacter setCharacterNFT={setCharacterNFT} />;
+    } else if (currentAccount && characterNFT) {
+      return (
+        <Arena characterNFT={characterNFT} setCharacterNFT={setCharacterNFT} />
+      );
     }
   };
 
