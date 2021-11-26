e786c465b2d323b1d9e5e0aa2d75eb6ff5e4aa2b
Zeha Irawan <zehairawan@gmail.com>
2021-11-27 07:39:59 +0800

    add select character component

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+const CONTRACT_ADDRESS = '0x4BD989457B8fcaf6b83bc1dF31e9ce16ad48a9f6';
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+export { CONTRACT_ADDRESS, transformCharacterData };
